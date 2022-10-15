fc2109780001c50b8254f7a99dc68f51525d5a6c
Anderson Correa <andersonfpcorrea@gmail.com>
2022-10-15 03:47:53 -0300

    update stack exercises description

--- stack/exercises.js
+++ stack/exercises.js
@@ -3,8 +3,8 @@ import Stack from './Stack.js';
 /*
 1. Uma stack pode ser usada para garantir que uma expressão matemática não tem erros em parênteses
 (ou seja, que não haja parênteses faltantes: "1 + (2 / 2 * 3" é uma expressão falha).
-Escreva uma função que recebe como argumento uma expressão aritmética e retorna a posição em
-que há um parênteses faltante.
+Escreva uma função que receba como argumento uma expressão aritmética e retorne a posição do parêntese
+não balanceado.
 */
 
 /*
