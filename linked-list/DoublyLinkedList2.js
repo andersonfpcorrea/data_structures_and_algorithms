52cabd0cd0cd55338312ed2a563f1fbe823c0672
Anderson Correa <andersonfpcorrea@gmail.com>
2022-12-02 16:35:07 -0300

    feat: create doublylinkedlist2 class

--- linked-list/DoublyLinkedList2.js
+++ linked-list/DoublyLinkedList2.js
@@ -3,41 +3,63 @@ import Node from './Node.js';
 import { defaultEquals } from '../utils.js';
 
 export default class DoublyLinkedList2 extends LinkedList2 {
-  #head;
-  #tail;
-  #count;
+  _head;
+  _tail;
+  _count;
 
   constructor(equalsFn = defaultEquals) {
     super(equalsFn);
-    this.#tail = null;
-    this.#head = null;
-    this.#count = 0;
+    this._tail = null;
+    this._head = null;
+    this._count = 0;
+  }
+
+  push(element) {
+    const node = new Node(element);
+    let current;
+    // Se a lista está vazia:
+    if (this._head === null) {
+      this._head = node;
+      this._tail = node;
+    }
+    // Se a lista não está vazia
+    else {
+      current = this._tail;
+      current.next = node;
+      node.previous = current;
+      this._tail = node;
+    }
+    this._count++;
   }
 
   insert(element, index) {
-    if (index < 0 || index > this.#count) return false;
+    console.log(index < 0);
+    console.log('index = ' + index);
+    console.log('this._count = ' + this._count);
+    console.log(index >= this._count);
+    if (index < 0 || index >= this._count) return false;
 
     const node = new Node(element);
-    let current = this.#head;
+    let current = this._head;
     // Adição ao 'head' da fila
     if (index === 0) {
       // Se index é 0, adiciona o 'node' ao 'head' - caso ele seja null
       // ou define 'node' como head:
-      if (this.#head === null) {
-        this.#head = node;
-        this.#tail = node;
+      if (this._head === null) {
+        this._head = node;
+        this._tail = node;
       } else {
-        node.next = this.#head;
-        current.prev = node;
-        this.#head = node;
+        node.next = this._head;
+        current.previous = node;
+        this._head = node;
       }
     }
     // Adição ao fim da lista:
-    else if (index === this.#count) {
-      current = this.#tail;
+    else if (index === this._count) {
+      current = this._tail;
       current.next = node;
       node.previous = current;
-      this.#tail = node;
+      this._tail = node;
     }
     // Adição em qualquer outra posição:
     else {
@@ -49,9 +71,45 @@ export default class DoublyLinkedList2 extends LinkedList2 {
       node.previous = previous;
     }
     // Após a inserção, atualiza o contador e retorna 'true':
-    this.#count++;
+    this._count++;
     return true;
   }
 
-  // * Continuar da implementação de removeAt(index)
+  removeAt(index) {
+    // Se 'index' não é válido retorna 'null'
+    if (index < 0 || index >= this._count) return null;
+
+    let current = this._head;
+
+    // Removendo do início da fila
+    if (index === 0) {
+      this._head = current.next;
+      // Se há apenas um element, atualizamos o 'tail':
+      if (this._count === 1) {
+        this._tail = null;
+      } else {
+        this._head.previous = null;
+      }
+    }
+
+    // Removendo do fim da fila
+    else if (index === this._count - 1) {
+      current = this._tail;
+      this._tail = current.previous;
+      this._tail.next = null;
+    }
+
+    // Removendo do meio da fila
+    else {
+      current = this.getElementAt(index);
+      const previous = current.previous;
+      // Associa 'previous.next' com 'current.next', removendo o elemento:
+      previous.next = current.next;
+      current.next.previous = previous;
+    }
+
+    // Atualiza o contador e retorna o elemento removido
+    this._count--;
+    return current.element;
+  }
 }
--- linked-list/LinkedList2.js
+++ linked-list/LinkedList2.js
@@ -2,23 +2,23 @@ import Node from './Node.js';
 import { defaultEquals } from '../utils.js';
 
 export default class LinkedList2 {
-  #count;
-  #head;
-  #equalsFn;
+  _count;
+  _head;
+  _equalsFn;
 
   constructor(equalsFn = defaultEquals) {
-    this.#count = 0;
-    this.#head = null;
-    this.#equalsFn = equalsFn;
+    this._count = 0;
+    this._head = null;
+    this._equalsFn = equalsFn;
   }
 
   push(element) {
     const node = new Node(element);
     let current;
-    if (this.#head === null) {
-      this.#head = node;
+    if (this._head === null) {
+      this._head = node;
     } else {
-      current = this.#head;
+      current = this._head;
       while (current.next !== null) {
         // Adquire o último element
         current = current.next;
@@ -26,14 +26,14 @@ export default class LinkedList2 {
       // Atribui 'next' do 'current' à 'node'
       current.next = node;
     }
-    this.#count++;
+    this._count++;
   }
 
   getElementAt(index) {
     // Retorna 'null' caso 'index' esteja fora dos limites da lista
-    if (index < 0 || index >= this.#count) return null;
+    if (index < 0 || index >= this._count) return null;
 
-    let node = this.head;
+    let node = this._head;
     // Itera pela lista até o indice desejado, quando 'node' será o elemento buscado
     for (let i = 0; i < index && node !== null; i++) {
       node = node.next;
@@ -43,13 +43,13 @@ export default class LinkedList2 {
 
   removeAt(index) {
     // Retorna 'null' caso 'index' esteja fora dos limites da lista
-    if (index < 0 || index >= this.#count) return null;
+    if (index < 0 || index >= this._count) return null;
 
     // Caso index seja válido, acha o elemento e o desassocia da lista:
-    let current = this.#head;
+    let current = this._head;
 
     if (index === 0) {
-      this.#head = current.next;
+      this._head = current.next;
     } else {
       const previous = this.getElementAt(index - 1);
       current = previous.next;
@@ -57,36 +57,36 @@ export default class LinkedList2 {
       previous.next = current.next;
     }
     // Atualiza o contador:
-    this.count--;
+    this._count--;
     // Retorna o elemento 'removido' (ou desassociado da lista):
     return current.element;
   }
 
   insert(element, index) {
     // Retorna 'false' caso 'index' esteja fora dos limites da lista
-    if (index < 0 || index >= this.#count) return false;
+    if (index < 0 || index >= this._count) return false;
 
     const node = new Node(element);
 
     if (index === 0) {
-      const current = this.#head;
+      const current = this._head;
       node.next = current;
-      this.#head = node;
+      this._head = node;
     } else {
       const previous = this.getElementAt(index - 1);
       const current = previous.next;
       node.next = current;
       previous.next = node;
     }
-    this.#count++;
+    this._count++;
     return true;
   }
 
   indexOf(element) {
-    let current = this.#head;
-    for (let i = 0; i < this.#count && current !== null; i++) {
+    let current = this._head;
+    for (let i = 0; i < this._count && current !== null; i++) {
       // Se 'current' e 'element' são iguais, retorna o índice de 'current':
-      if (this.#equalsFn(element, current.element)) {
+      if (this._equalsFn(element, current.element)) {
         return i;
       }
       current = current.next;
@@ -102,7 +102,7 @@ export default class LinkedList2 {
   }
 
   size() {
-    return this.#count;
+    return this._count;
   }
 
   isEmpty() {
@@ -112,8 +112,8 @@ export default class LinkedList2 {
   toString() {
     if (this.isEmpty()) return '';
 
-    let objString = `${this.#head.element}`;
-    let current = this.#head.next;
+    let objString = `${this._head.element}`;
+    let current = this._head.next;
     for (let i = 1; i < this.size() && current !== null; i++) {
       objString = `${objString}\n${current.element}`;
       current = current.next;
@@ -122,6 +122,10 @@ export default class LinkedList2 {
   }
 
   get head() {
-    return this.#head;
+    return this._head;
+  }
+
+  get count() {
+    return this._count;
   }
 }
