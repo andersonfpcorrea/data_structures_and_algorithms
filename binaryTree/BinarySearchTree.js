e9a6663b6963383ffc6035deb3009d6e12c0bd22
Anderson Correa <andersonfpcorrea@gmail.com>
2023-05-20 21:45:07 -0300

    feat: binary-search-tree in-rder-traverse

---
+++ binaryTree/BinarySearchTree.js
@@ -0,0 +1,50 @@
+import { Compare, defaultCompare } from '../utils.js';
+import { Node } from './Node.js';
+
+export default class BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    this.compareFn = compareFn; // used to compare node values
+    this.root = null; // root node of type Node
+  }
+
+  insert(key) {
+    if (this.root === null) {
+      this.root = new Node(key);
+    } else {
+      this.insertNode(this.root, key);
+    }
+  }
+
+  insertNode(node, key) {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left === null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = new Node(key);
+      } else {
+        this.insertNode(node.right, key);
+      }
+    }
+  }
+
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback);
+  }
+
+  inOrderTraverseNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      this.inOrderTraverseNode(node.right, callback);
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(11);
+const printNode = (value) => console.log(value);
+tree.inOrderTraverse(printNode);
---
+++ binaryTree/Node.js
@@ -0,0 +1,7 @@
+export class Node {
+  constructor(key) {
+    this.key = key; // Node value
+    this.left = null; // left child node referece
+    this.right = null; // right child node referece
+  }
+}
